a26e134feb58d74e4bbdc371bd6ac9aac872bf40
DragonPickles <jennyrodriguez843@gmail.com>
2017-10-28 16:39:21 -0700

    Death Menu

---
+++ DontStopMeNow/Assets/Scripts/DeathMenu.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeathMenu : MonoBehaviour {
+
+    public PlayerController player;
+    public GameObject menu;
+
+    // Use this for initialization
+    void Start()
+    {
+        player = GameObject.Find("Player").GetComponent<PlayerController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        menu.SetActive(player.isPlayerDead());
+    }
+}
