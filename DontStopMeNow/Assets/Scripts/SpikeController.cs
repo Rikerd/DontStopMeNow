135f4f613d0ac3d367f8ed5e277871c5f82324e2
Kevin Touch <ktouch@uci.edu>
2017-10-29 18:19:11 -0700

    Work on Level 3
    WIP

--- DontStopMeNow/Assets/Scripts/SpikeController.cs
+++ DontStopMeNow/Assets/Scripts/SpikeController.cs
@@ -8,10 +8,13 @@ public class SpikeController : MonoBehaviour {
     public float direction = 1;
     public bool up;
     public LayerMask playerLayer;
+    public bool horizontal;
+    public float detectionRange; 
 
     private GameObject player;
     private RaycastHit2D ray;
     private bool moving;
+    private bool horizontalMoving;
 
     // Use this for initialization
     void Start () {
@@ -28,14 +31,31 @@ public class SpikeController : MonoBehaviour {
         {
             transform.Translate(Vector2.up * movementSpeed * direction * Time.deltaTime);
         }
+
+        if (horizontalMoving)
+        {
+            transform.Translate(Vector2.left * movementSpeed * Time.deltaTime);
+        }
     }
 
     void checkPlayer()
     {
-        ray = Physics2D.Raycast(transform.position - offset, Vector2.up * direction, 10f, playerLayer);
-        if (ray.collider != null && (player.GetComponent<Rigidbody2D>().gravityScale * direction < 0))
+        if (horizontal)
         {
-            moving = true;
+            ray = Physics2D.Raycast(transform.position, Vector2.left, detectionRange, playerLayer);
+            if (ray.collider != null)
+            {
+                horizontalMoving = true;
+            }
         }
+        else
+        {
+            ray = Physics2D.Raycast(transform.position - offset, Vector2.up * direction, 10f, playerLayer);
+            if (ray.collider != null && (player.GetComponent<Rigidbody2D>().gravityScale * direction < 0))
+            {
+                moving = true;
+            }
+        }
+        
     }
 }
